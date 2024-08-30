730080da0857dc8c1437956f21838a238e2b4d86
Erik Soehnel <eriksoehnel@gmail.com>
2024-08-30 13:57:58 +0200

    use mantine app shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,126 @@
-import { Button, Tabs, Title } from "@mantine/core";
+import { Anchor, AppShell, Burger, Button, Group, Tabs } from "@mantine/core";
 import "@mantine/core/styles.css";
+import { useDisclosure } from "@mantine/hooks";
 import React from "react";
 import styles from "./App.module.css";
 import { Editor, getJsCode } from "./components/Editor";
 import { Result } from "./components/Result";
 import { Sql } from "./components/Sql";
 
-export const App: React.VFC = () => {
+const PlaygroundMain: React.VFC = () => {
   const [width, setWidth] = React.useState(
     (window.visualViewport?.width || 0) / 2,
   );
   const [isMoving, setIsMoving] = React.useState(false);
 
   return (
-    <>
-      <Title className={styles.Header}>Typesafe Query Builder Playground</Title>
-      <Button onClick={runCode}>RUN</Button>
-      <div className={styles.Panels}>
-        <div className={styles.Editor}>
-          <Tabs variant="outline" defaultValue="example">
-            <Tabs.List pl="lg">
-              <Tabs.Tab value="example">example.ts</Tabs.Tab>
-            </Tabs.List>
+    <div className={styles.Panels}>
+      <div className={styles.Editor}>
+        <Tabs variant="outline" defaultValue="example">
+          <Tabs.List pl="lg">
+            <Tabs.Tab value="example">example.ts</Tabs.Tab>
+          </Tabs.List>
 
-            <Tabs.Panel value="example">
-              <Editor size={width} />;
-            </Tabs.Panel>
-          </Tabs>
-        </div>
-        <div
-          className={styles.Divider}
-          onPointerDown={(ev) => {
-            ev.currentTarget.setPointerCapture(ev.pointerId);
-            setIsMoving(true);
-          }}
-          onPointerUp={(ev) => {
-            ev.currentTarget.releasePointerCapture(ev.pointerId);
-            setIsMoving(false);
-          }}
-          onPointerMove={(ev) => {
-            if (isMoving) {
-              const newWidth = Math.max(
-                200,
-                Math.min((window.visualViewport?.width || 0) - 200, ev.clientX),
-              );
-
-              setWidth(newWidth);
-            }
-          }}
-        />
-        <div className={styles.Result}>
-          <Tabs variant="outline" defaultValue="result">
-            <Tabs.List pl="lg">
-              <Tabs.Tab value="result">Result</Tabs.Tab>
-              <Tabs.Tab value="sql">SQL</Tabs.Tab>
-              <Tabs.Tab value="log">Log</Tabs.Tab>
-            </Tabs.List>
-
-            <Tabs.Panel value="result">
-              <Result />
-            </Tabs.Panel>
-
-            <Tabs.Panel value="sql">
-              <Sql />
-            </Tabs.Panel>
-
-            <Tabs.Panel value="log">LOG</Tabs.Panel>
-          </Tabs>
-        </div>
+          <Tabs.Panel value="example">
+            <Editor size={width} />;
+          </Tabs.Panel>
+        </Tabs>
       </div>
-    </>
+      <div
+        className={styles.Divider}
+        onPointerDown={(ev) => {
+          ev.currentTarget.setPointerCapture(ev.pointerId);
+          setIsMoving(true);
+        }}
+        onPointerUp={(ev) => {
+          ev.currentTarget.releasePointerCapture(ev.pointerId);
+          setIsMoving(false);
+        }}
+        onPointerMove={(ev) => {
+          if (isMoving) {
+            const newWidth = Math.max(
+              200,
+              Math.min((window.visualViewport?.width || 0) - 200, ev.clientX),
+            );
+
+            setWidth(newWidth);
+          }
+        }}
+      />
+      <div className={styles.Result}>
+        <Tabs variant="outline" defaultValue="result">
+          <Tabs.List pl="lg">
+            <Tabs.Tab value="result">Result</Tabs.Tab>
+            <Tabs.Tab value="sql">SQL</Tabs.Tab>
+            <Tabs.Tab value="log">Log</Tabs.Tab>
+          </Tabs.List>
+
+          <Tabs.Panel value="result">
+            <Result />
+          </Tabs.Panel>
+
+          <Tabs.Panel value="sql">
+            <Sql />
+          </Tabs.Panel>
+
+          <Tabs.Panel value="log">LOG</Tabs.Panel>
+        </Tabs>
+      </div>
+    </div>
   );
 };
+
+// MobileNavbar layout https://mantine.dev/app-shell/?e=MobileNavbar
+export function App() {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { desktop: true, mobile: !opened },
+      }}
+      padding=""
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <Group justify="space-between" style={{ flex: 1 }}>
+            <Group>
+              Typesafe Query Builder Playground
+              <Button onClick={runCode}>RUN</Button>
+            </Group>
+            <Group ml="xl" gap={0} visibleFrom="sm">
+              <Anchor
+                className={styles.AppShellButton}
+                href="https://github.com/hoeck/typesafe-query-builder"
+                target="_blank"
+              >
+                Github
+              </Anchor>
+            </Group>
+          </Group>
+        </Group>
+      </AppShell.Header>
+
+      <AppShell.Navbar py="md" px={4}>
+        <Anchor
+          className={styles.AppShellButton}
+          href="https://github.com/hoeck/typesafe-query-builder"
+          target="_blank"
+        >
+          Github
+        </Anchor>
+      </AppShell.Navbar>
+
+      <AppShell.Main>
+        <PlaygroundMain />
+      </AppShell.Main>
+    </AppShell>
+  );
+}
 
 async function runCode() {
   (globalThis as any).sqlLogReset();
